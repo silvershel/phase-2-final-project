df919bb6f9cd7cf3fcc51a696736f8080f57f268
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-04-24 13:53:00 -0400

    updated files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Search from "./Search";
 import ProjectList from "./ProjectList";
 
 function App() {
+    const [projects, setProjects] = useState([])
 
+    useEffect(() => {
+        fetch("http://localhost:3000/")
+        .then((r) => r.json())
+        .then((projects) => setProjects(projects))
+    })
+
+    return (
+    <div className="app">
+        <Header />
+        <Search />
+        <ProjectList projects={projects} />
+    </div>
+    )
 }
 
 export default App;

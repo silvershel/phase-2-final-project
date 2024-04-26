5dc388d773a8b4cd7398db0ade7b266f459da618
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-04-26 13:01:41 -0400

    add image element to ProjectCard component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,14 @@
 function ProjectCard({ project }) {
 
     // Object Destructuring Assignment
-    const { name, designer, pattern } = project;
+    const { name, designer, pattern, image } = project;
 
     return (
         <div>
             <h1>{name}</h1>
             <p>Designer: {designer}</p>
             <p>Pattern: {pattern}</p>
+            <img src={image} />
         </div>
     )
 

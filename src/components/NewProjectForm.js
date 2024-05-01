5a6c8147c1fe8e8480816d1f4333ffdb061e622d
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-05-01 11:53:51 -0400

    added form elements

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -2,6 +2,23 @@ function NewProjectForm() {
     return (
         <div>
            <p>New Project</p>
+           <form>
+                <label>
+                    Text Input:
+                    <input type="text" value="One"/>
+                    Text Area:
+                    <textarea value="text area"/>
+                    Dropdown:
+                    <select>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                </label> 
+                                  
+                <button type="submit">Submit</button>
+           </form>
         </div>
     )
 }
